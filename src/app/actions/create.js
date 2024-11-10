d50d778d7f76f4489f6356268ead86d1d8b8fe93
pongpatapee <pongpatapee1021@gmail.com>
2024-11-10 15:16:16 -0500

    feat: can add book to redis

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -1,10 +1,26 @@
-'use server'
+"use server";
 
-// import { client } from "@/lib/db"
-import { redirect } from 'next/navigation'
+import { client } from "../lib/db";
+import { redirect } from "next/navigation";
 
 export async function createBook(formData) {
-  const {title, rating, author, blurb} = Object.fromEntries(formData)
+  const { title, rating, author, blurb } = Object.fromEntries(formData);
 
-  
+  // create book id (in a real application UUID would be better)
+  const id = Math.floor(Math.random() * 100_000);
+
+  console.log(`Sending book ${id} to redis`)
+
+  // save book as redis hash
+  await client.hSet(`books:${id}`, {
+    title,
+    rating,
+    author,
+    blurb,
+  });
+
+  console.log("Done sending to redis")
+
+  // redirect doesn't work right now
+  // redirect("/");
 }
